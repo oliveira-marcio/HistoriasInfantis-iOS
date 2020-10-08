0b47243f8b4f216f1e67e87c9f0636bdf8689480
oliveira-marcio <emaildomso@gmail.com>
2020-10-08 14:21:23 +0100

    refactor: Small refactor to files

--- HistoriasInfantis/Data/StoriesRepositoryImplementation.swift
+++ HistoriasInfantis/Data/StoriesRepositoryImplementation.swift
@@ -1,24 +1,10 @@
 //
-//  StoriesRepository.swift
+//  StoriesRepositoryImplementation.swift
 //  HistoriasInfantis
 //
 //  Created by Márcio Oliveira on 10/1/20.
 //  Copyright © 2020 Márcio Oliveira. All rights reserved.
 //
-
-
-typealias StoriesRepositoryFetchAllCompletionHandler = (Result<[Story]>) -> Void
-
-public enum StoriesRepositoryError: Error, Equatable {
-    case gatewayRequestFail(String)
-    case gatewayParseFail(String)
-    case unableToSave
-}
-
-protocol StoriesRepository {
-    func fetchAll(then handler: @escaping StoriesRepositoryFetchAllCompletionHandler)
-    func requestNew(then handler: @escaping StoriesRepositoryFetchAllCompletionHandler)
-}
 
 class StoriesRepositoryImplementation: StoriesRepository {
     private let storiesLocalGateway: StoriesLocalGateway!
---
+++ HistoriasInfantis/Domain/StoriesRepository.swift
@@ -0,0 +1,20 @@
+//
+//  StoriesRepository.swift
+//  HistoriasInfantis
+//
+//  Created by Márcio Oliveira on 10/8/20.
+//  Copyright © 2020 Márcio Oliveira. All rights reserved.
+//
+
+typealias StoriesRepositoryFetchAllCompletionHandler = (Result<[Story]>) -> Void
+
+public enum StoriesRepositoryError: Error, Equatable {
+    case gatewayRequestFail(String)
+    case gatewayParseFail(String)
+    case unableToSave
+}
+
+protocol StoriesRepository {
+    func fetchAll(then handler: @escaping StoriesRepositoryFetchAllCompletionHandler)
+    func requestNew(then handler: @escaping StoriesRepositoryFetchAllCompletionHandler)
+}
